9df02b20dd1c2b6a6a6da3d301918ebd20c8846e
Lasha Sulaberidze <lsulaberidze@tbcbank.com.ge>
2022-03-04 21:56:58 +0400

    Update winners component

--- apps/f1-champions/src/app/features/winners-of-year/winners-of-year.component.ts
+++ apps/f1-champions/src/app/features/winners-of-year/winners-of-year.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ErgastApiService, SingleRace } from '@mobiquity-workspace/ergast';
+import { map, Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'f1-champions-winners-of-year',
@@ -6,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./winners-of-year.component.scss'],
 })
 export class WinnersOfYearComponent implements OnInit {
-  constructor() {}
+  public races?: Observable<SingleRace[]>;
+  constructor(
+    private ergastApiService: ErgastApiService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const year = this.route.snapshot.paramMap.get('year') || '';
+    this.races = this.ergastApiService
+      .getRaceWinnersByYear(year)
+      .pipe(map((r) => r.RaceTable.Races.map((race) => new SingleRace(race))));
+  }
 }
