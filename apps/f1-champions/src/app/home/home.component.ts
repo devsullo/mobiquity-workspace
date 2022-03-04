93041ebb8c72a5985e4cde6d4465188a0295b260
Lasha Sulaberidze <lsulaberidze@tbcbank.com.ge>
2022-03-04 21:07:07 +0400

    Update home component

--- apps/f1-champions/src/app/home/home.component.ts
+++ apps/f1-champions/src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FirstStanding, ErgastApiService } from '@mobiquity-workspace/ergast';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'f1-champions-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  public firstStandings?: Observable<FirstStanding[]>;
+  constructor(private ergastApiService: ErgastApiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.firstStandings = this.ergastApiService
+      .getAllYearWinners()
+      .pipe(
+        map((r) =>
+          r.StandingsTable.StandingsLists.map((s) => new FirstStanding(s))
+        )
+      );
+  }
 }
