06ea774563636090ed01fef834201adac739b845
Lasha Sulaberidze <lsulaberidze@tbcbank.com.ge>
2022-03-07 02:39:35 +0400

    E2E tests

--- apps/f1-champions-e2e/src/integration/app.spec.ts
+++ apps/f1-champions-e2e/src/integration/app.spec.ts
@@ -1,13 +1,33 @@
-import { getGreeting } from '../support/app.po';
+import {
+  getGreeting,
+  firstElementInRow,
+  highlightedRow,
+} from '../support/app.po';
 
 describe('f1-champions', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display title', () => {
+    getGreeting().contains('Champions').and('be.visible');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome f1-champions');
+  it('should have 2005 year winner on top of list', () => {
+    firstElementInRow().children().should('contain', '2005').and('be.visible');
+  });
+
+  it('should navigate to correct path', () => {
+    firstElementInRow().click();
+    cy.url().should('contain', 'winners-of-year/2005');
+  });
+
+  it('should display a right title', () => {
+    firstElementInRow().click();
+    getGreeting().contains('Race Winners 2005').and('be.visible');
+  });
+
+  it('should highlight world champion correctly', () => {
+    firstElementInRow().click();
+    highlightedRow().should('contain', 'Fernando Alonso');
+    highlightedRow().should('have.length', 7);
   });
 });
--- apps/f1-champions-e2e/src/support/app.po.ts
+++ apps/f1-champions-e2e/src/support/app.po.ts
@@ -1,1 +1,3 @@
 export const getGreeting = () => cy.get('h1');
+export const firstElementInRow = () => cy.get('p-table tr').first();
+export const highlightedRow = () => cy.get('p-table').find('.h-row');
